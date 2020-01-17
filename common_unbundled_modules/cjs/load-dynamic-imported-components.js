e84d7f72f02a1a3d5e2bfe6f756fb228250234c9
artem gruden <artemxgruden@gmail.com>
2020-01-17 11:39:19 +0300

    1.0.0-alpha.2:init

---
+++ common_unbundled_modules/cjs/load-dynamic-imported-components.js
@@ -0,0 +1,11 @@
+const loadDynamicImportedComponents = async (dynamicImportedComponents) => {
+	if (dynamicImportedComponents && Array.isArray(dynamicImportedComponents)) {
+		for (const dynamicImportedComponent of dynamicImportedComponents) {
+			if (dynamicImportedComponent && dynamicImportedComponent.load) {
+				await dynamicImportedComponent.load();
+			}
+		}
+	}
+};
+
+module.exports = loadDynamicImportedComponents;
--- common_unbundled_modules/load-dynamic-imported-components.js
+++
@@ -1,9 +0,0 @@
-export default async (dynamicImportedComponents) => {
-  if (dynamicImportedComponents && Array.isArray(dynamicImportedComponents)) {
-    for (const dynamicImportedComponent of dynamicImportedComponents) {
-      if (dynamicImportedComponent && dynamicImportedComponent.load) {
-        await dynamicImportedComponent.load();
-      };
-    };
-  };
-};
